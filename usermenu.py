cce0448070e2eb3e62cf32d840aa4c20a4baf0ec
Maitreyi Shrinivas <64851181+mai3yi@users.noreply.github.com>
2026-01-04 05:13:31 -0500

    finished option one

--- usermenu.py
+++ usermenu.py
@@ -1,3 +1,4 @@
+entries = []
 
 print ("Welcome to the User Log System.")
 # criteria: Create a menu system that acts like a small database of user input that is searchable.
@@ -10,17 +11,45 @@ print ("-----------------------------------")
 print ("Please type the number corresponding to your choice:")
 print ("-----------------------------------")
 
-user_input = input()
-if user_input == "1":
-        print ("You have selected Add Entry.")
-        #code for adding an entry here
-elif user_input == "2":
-        print ("You have selected Search Entries.")
-        #code for searching entries here
-elif user_input == "3":
-        print ("You have selected View All Entries.")
-        #code for viewing entries here
-elif user_input == "4":
-        print ("Exiting program. Bye!")
-else:
-    print("Invalid Input. Try again.")
+while True:
+        user_input = input()
+        if user_input == "1":
+                lines = []
+                print ("You have selected Add Entry.")
+                print ("Please enter your entry below.")
+                print (" Type //done to save. Type //back to go back to the main menu.")
+                while True:
+                        user_input = input()
+                        disallow = user_input.strip()
+                        
+                        if user_input == "//done":      
+                                if lines == []:
+                                        print ("You must type at least one character.")
+                                        continue
+                                else:
+                                        full_entry = "\n".join(lines)
+                                        entries.append(full_entry)
+                                        print ("Entry saved.")
+                                        break
+                        elif user_input == "//back":
+                                break
+                        
+                        elif  disallow == "":
+                                print ("You must type at least one character.")
+                                continue
+                        else:
+                                lines.append(user_input)
+           
+                   
+                #code for adding an entry here
+        elif user_input == "2":
+                print ("You have selected Search Entries.")
+                #code for searching entries here
+        elif user_input == "3":
+                print ("You have selected View All Entries.")
+                #code for viewing entries here
+        elif user_input == "4":
+                print ("Exiting program. Bye!")
+                break
+        else:
+                print("Invalid Input. Try again.")
